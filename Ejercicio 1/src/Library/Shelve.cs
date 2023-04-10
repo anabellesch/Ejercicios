2ef02ae47566836d6f077d892bcf9b7ad7b31734
anabellesch <127800156+anabellesch@users.noreply.github.com>
2023-04-10 20:23:29 -0300

    Add files via upload

---
+++ Ejercicio 1/src/Library/Shelve.cs
@@ -0,0 +1,18 @@
+using System;
+public class Shelve {
+
+    public string LibrarySector { get ; set; }
+    public string LibraryShelve { get ; set; }
+
+    public void ShelveBook(String sector, String shelve)
+        {
+            this.LibrarySector = sector;
+            this.LibraryShelve = shelve;
+        }
+
+        
+           
+
+
+}
+
