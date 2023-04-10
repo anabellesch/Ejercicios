5fa9e9ddb5ec6e4e2d5d510b198148277b8d47f8
anabellesch <127800156+anabellesch@users.noreply.github.com>
2023-04-10 20:44:06 -0300

    Create Sector.cs

---
+++ Ejercicio 1/src/Library/Sector.cs
@@ -0,0 +1,12 @@
+using System;
+namespace SRP;
+
+
+ public class Sector
+    {
+        public string LibrarySector { get ; set; }
+        public Shelve(String sector)
+        {
+            this.LibrarySector = sector;
+        }
+    }
