a3d73637af24001079a91a334946c2d94b116c76
anabellesch <127800156+anabellesch@users.noreply.github.com>
2023-04-12 19:35:26 -0300

    Create doctor.cs

---
+++ Ejercicio 2/src/Library/doctor.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace doctor 
+{
+
+
+
+
+}
+
+    public class doctor_ 
+    {
+        public string DName { get; } 
+        public string DSpecialization { get; }
+        public doctor_(String DName, string DSpecialization)
+        {
+            this.DName = DName; 
+            this.DSpecialization = DSpecialization; 
+        }
+
+
+    }
