dfebf8132ab6452f5f84da91d33b54e1adbfd93b
anabellesch <127800156+anabellesch@users.noreply.github.com>
2023-04-12 19:34:55 -0300

    Create person.cs

---
+++ Ejercicio 2/src/Library/person.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace person 
+{
+
+
+
+
+}
+
+    public class person_ 
+    {
+        public string Name { get; } 
+        public string id { get; }
+        public string PhoneNumber { get; }
+        public person_(String Name, string id, string phoneNumber)
+        {
+            this.Name = Name; 
+            this.id = id; 
+            this.PhoneNumber = PhoneNumber;
+        }
+
+
+    }
