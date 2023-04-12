11c8e83edfe9079f091c3a0f9b0d039457690b95
anabellesch <127800156+anabellesch@users.noreply.github.com>
2023-04-12 19:36:30 -0300

    Update Program.cs

--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Library;
 
 namespace Program
@@ -6,12 +6,15 @@ namespace Program
     class Program
     {
         static void Main(string[] args)
+    
         {
-            string appointmentResult = AppointmentService.CreateAppointment("Steven Jhonson", "986782342", "5555-555-555", DateTime.Now, "Wall Street", "Armand");
+            person_ person=new person_("Anabelle", "12345", "098765432");
+            doctor_ doctor= new doctor_ ("Doctor","Especializacion");
+            string appointmentResult = AppointmentService.CreateAppointment(person.Name, person.id, person.PhoneNumber, DateTime.Now, "Wall Street", doctor.DName);
             Console.WriteLine(appointmentResult);
 
-            string appointmentResult2 = AppointmentService.CreateAppointment("Ralf Manson", "", "5555-555-555", DateTime.Now, "Queen Street", "");
-            Console.WriteLine(appointmentResult2);
+           
         }
     }
 }
+
